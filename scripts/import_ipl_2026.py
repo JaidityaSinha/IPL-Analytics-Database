b6ebba7d97978a2a4d43921558a18c0b8e471060
JaidityaSinha <jaidityasinha200@gmail.com>
2026-07-04 00:34:28 +0530

    Implement team data import

--- scripts/import_ipl_2026.py
+++ scripts/import_ipl_2026.py
@@ -1,18 +1,46 @@
+import pandas as pd
+
 from database import get_connection
 
 
+def load_teams(connection):
+    dataframe = pd.read_csv("../data/squads.csv")
+
+    cursor = connection.cursor()
+
+    team_ids = {}
+
+    teams = sorted(dataframe["team_name"].unique())
+
+    for team in teams:
+        cursor.execute(
+
+            """
+            INSERT INTO teams(team_name)
+            VALUES (%s)
+            RETURNING team_id;
+            """,
+            (team,)
+        )
+
+        team_id = cursor.fetchone()[0]
+        team_ids[team] = team_id
+
+    connection.commit()
+
+    cursor.close()
+
+    return team_ids
+
+
 def main():
-    try:
-        connection = get_connection()
+    connection = get_connection()
 
-        print("Connected to PostgreSQL successfully!")
+    team_ids = load_teams(connection)
 
-        connection.close()
+    print(team_ids)
 
-        print("Connection closed successfully!")
-
-    except Exception as e:
-        print(e)
+    connection.close()
 
 
 if __name__ == "__main__":
